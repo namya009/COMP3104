11ed600b3bcf5a30bbfb620f4565ad143234f17f
namya009 <namyapatel02@gmail.com>
2021-10-24 22:26:41 -0400

    Updated .travis configs

---
+++ build/precache-manifest.f8908679287a53897ce4af50f85bbb01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18ea84a865e09c21ec19",
+    "url": "/namya009/COMP3104/static/css/main.ac684b17.chunk.css"
+  },
+  {
+    "revision": "18ea84a865e09c21ec19",
+    "url": "/namya009/COMP3104/static/js/main.18ea84a8.chunk.js"
+  },
+  {
+    "revision": "9316371ffbdb474a0038",
+    "url": "/namya009/COMP3104/static/js/runtime~main.9316371f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/namya009/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "f1aaa6120c424f5099b20e2949412d69",
+    "url": "/namya009/COMP3104/index.html"
+  }
+];
